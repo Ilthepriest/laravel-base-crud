a5e3f58c4d8a42a34772ed4c4e78bb5663d21311
Ilthepriest <robertodelprete1993@libero.it>
2022-06-16 15:55:15 +0200

    error

---
+++ database/migrations/2022_06_16_130331_create_comics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nulllable();
+            $table->string('thumb')->nulllable();
+            $table->string('cover_image')->nulllable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics=[
+            'title' =>  'Topolino',
+            'description' =>  'Il fumetto di Topolino è uno dei più assurdi',
+            'thumb' =>  'https://static.wikia.nocookie.net/disney/images/d/d4/IMG_1868.PNG/revision/latest?cb=20161228161522&path-prefix=it',
+            'cover_image' =>  'https://www.lospaziobianco.it/wp-content/uploads/2021/09/Topolino-racconta-Dante-IMG-EVIDENZA.jpg',
+        ];
+
+        foreach($comics as $comic )
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->cover_image = $comic['cover_image'];
+            $new_comic->save();
+
+    }
+}
